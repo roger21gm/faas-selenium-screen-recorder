79dab55c437dbb6494f62eaffe91ee86fb6b77eb
Roger Generoso Masós <roger21gm@gmail.com>
2021-07-06 10:33:08 +0200

    add example script

--- example_script/script.py
+++ example_script/script.py
@@ -6,21 +6,24 @@ def handle():
     Args:
         req (str): request body
     """
-    options = webdriver.ChromeOptions()
+    try:
+        options = webdriver.ChromeOptions()
 
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
 
-    proxy = os.getenv('proxy', '')
-    options.add_argument('--proxy-server=%s' % proxy)
+        proxy = os.getenv('proxy', '')
+        options.add_argument('--proxy-server=%s' % proxy)
 
-    driver = webdriver.Chrome(chrome_options=options)
-    
-    driver.get("https://google.es")
-    screenshot = driver.get_screenshot_as_base64()
-    
-    driver.close()
-    driver.quit()
+        driver = webdriver.Chrome(chrome_options=options)
+        
+        driver.get("https://google.es")
+        screenshot = driver.get_screenshot_as_base64()
+    except Exception as e:
+        print(e)
+    finally:
+        driver.close()
+        driver.quit()
 
     return screenshot
 
